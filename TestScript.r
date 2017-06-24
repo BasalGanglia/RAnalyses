8ef33c30a38d25d1831249abb204708c0366fe2c
Ilkka Kosunen <ilkka.kosunen@helsinki.fi>
2017-06-24 12:08:20 +0300

    plotly

--- TestScript.r
+++ TestScript.r
@@ -58,3 +58,134 @@ df.sep <- df %>% separate(x, c("ABC", "XYZ"))
 
 ######## UNITE ##############
 unite(df.sep,new.joined.col,ABC,XYZ)
+
+
+######## Visualizations with GGplot2 ""#################
+install.packages("ggplot2")
+library(ggplot2)
+pl <- ggplot(data=mtcars,aes(x=mpg,y=hp))
+pl <- pl+geom_point()
+pl + facet_grid(cyl ~ .)
+
+
+pl <-ggplot(data=mtcars,aes(x=mpg,y=hp))+geom_point()
+pl2 <-pl + facet_grid(cyl ~ .) + stat_smooth()
+pl2 + coord_cartesian(xlim = c(15,25))
+
+
+######## HISTORGRAM Lecture ################
+
+
+install.packages('ggplot2movies')
+library(ggplot2movies)
+
+# Data & Aesthetics
+
+pl <- ggplot(movies,aes(x=rating))
+
+# Geometry
+
+pl2 <- pl + geom_histogram(binwidth=0.1, color='red', fill='pink',
+                          alpha=0.4)
+pl3 <- pl2 + xlab('Movie Ratings') + ylab('Count')
+print(pl3)
+
+print(pl3 + ggtitle('titteli'))
+
+############## Scatterplots ##############
+
+library(ggplot2)
+df <- mtcars
+
+# Data & Aesthetics
+
+pl <- ggplot(df, aes(x=wt,y=mpg))
+ 
+# Geometry
+
+pl + geom_point(size=5, alpha=0.5)
+print(pl + geom_point(aes(size=hp)))
+
+pl + geom_point(aes(size=factor(cyl)))
+pl2 <- pl + geom_point(aes(shape=factor(cyl),size=5,color=hp))
+pl2 + scale_color_gradient(low='blue', high='red')
+
+############# Barplots ##################
+
+df <-mpg
+
+pl <- ggplot(df, aes(x=class))
+
+print(pl + geom_bar(aes(fill=drv),position="dodge"))
+pl + geom_bar(aes(fill=drv), position="fill")
+
+####### Boxplots #################
+
+df <- mtcars
+
+pl <- ggplot(df, aes(x=cyl,y=mpg)) # Wrong
+pl + geom_boxplot()
+
+pl <- ggplot(df, aes(x = factor(cyl),y=mpg))
+pl2 <- pl + geom_boxplot()
+pl2 + coord_flip()
+
+pl + geom_boxplot((aes(fill=factor(cyl))))
+
+
+######## 2 Variable Plotting ################
+library(ggplot2)
+library(ggplot2movies)
+
+pl <- ggplot(movies,aes(x=year, y=rating))
+
+pl + geom_bin2d() + scale_fill_gradient(high='red',low='green')
+
+install.packages('hexbin')
+pl2 <- pl + geom_hex()
+pl2
+
+pl2 <- pl + geom_density2d()
+pl2
+
+
+####### coordinates and faceting
+
+
+pl <- ggplot(mpg,aes(x=displ, y=hwy)) + geom_point()
+pl2 <- pl + coord_cartesian(xlim=c(1,4),ylim=c(15,30))
+pl3 <- pl + coord_fixed(ratio=1/3)
+pl3
+
+pl + facet_grid(. ~cyl)
+
+pl + facet_grid(drv ~ .)
+
+pl + facet_grid(drv ~ cyl)
+
+############### Themes ################
+
+theme_set(theme_minimal())
+pl <- ggplot(mtcars, aes(x=wt, y=mpg)) +geom_point()
+pl
+
+pl + theme_dark()
+
+install.packages('ggthemes')
+library(ggthemes)
+
+pl + theme_calc()
+pl + theme_pander()
+
+
+###### Plot.ly ###############
+
+
+install.packages('plotly')
+library(plotly)
+
+pl <- ggplot(mtcars, aes(mpg,wt)) + geom_point()
+
+pl
+gpl <- ggplotly(pl)
+gpl
